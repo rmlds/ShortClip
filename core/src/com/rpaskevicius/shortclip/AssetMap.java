ad75b86be8b5c7fbff499823fcc251c6f9d8c870
unknown <romualdaspaskevicius@gmail.com>
2019-04-02 17:53:40 +0100

    Assets v2

--- core/src/com/rpaskevicius/shortclip/AssetMap.java
+++ core/src/com/rpaskevicius/shortclip/AssetMap.java
@@ -6,24 +6,18 @@ public class AssetMap {
         "closed-hihat-01.wav",
         "closed-hihat-02.wav",
         "kick-01.wav",
-        "kick-02.wav",
-        "kick-03.wav",
-        "open-hihat-01.wav",
-        "shaker-01.wav",
-        "shaker-02.wav",
-        "shaker-03.wav",
-        "snare-01.wav",
-        "snare-02.wav",
-        "tambourine-01.wav"
+        "clap-01.wav"
     };
 
     public static final String[] instruments = new String[] {
-        "bass",
-        "piano"
+        "warm-bass",
+        "lofi-chords",
+        "warm-pad",
+        "soft-stab"
     };
 
     public static final String defaultSound = "kick-01.wav";
-    public static final String defaultInstrument = "piano";
+    public static final String defaultInstrument = "lofi-chords";
 
     public static String getSoundString(int index) {
         return sounds[index];
